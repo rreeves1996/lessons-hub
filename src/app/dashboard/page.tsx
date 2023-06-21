5936ecac041ab2d935c166784f7a6f2812d8c7e0
Ryan Reeves <reeves0ryan@gmail.com>
2023-06-20 18:48:31 -0700

    Add react-icons, begin hamburger menu (and comment out for the moment)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Upcoming from './Upcoming';
 
 export default function Dashboard() {
 	return (
 		<>
-			<header className='w-full flex'>
-				<div className='w-[50%] flex justify-center'>hello</div>
-				<div className='w-[50%] flex justify-center'>hello</div>
+			<header className='w-full flex flex-col md:flex-row'>
+				<div className='w-full flex justify-center'>
+					<Upcoming />
+				</div>
+				<div className='w-full flex justify-center'>hello</div>
 			</header>
 		</>
 	);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,45 @@
+'use client';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import React from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 export default function Navbar() {
+	const [collapsed, setCollapsed] = useState<boolean>(true);
+
 	return (
 		<nav className='w-full'>
 			<div className='max-w-7xl mx-auto flex justify-between py-2 px-4'>
 				<header>
 					<h3 className='text-3xl'>lessonHUB</h3>
 				</header>
-				<ul className='flex flex-col gap-4'>
-					<li>
-						<Link href='/dashboard'>Home</Link>
-					</li>
-					<li>
-						<Link href='/dashboard'>Students</Link>
-					</li>
-					<li>
-						<Link href='/dashboard'>Schedule</Link>
-					</li>
-					<li>
-						<Link href='/dashboard'>My Account</Link>
-					</li>
-					<li>
-						<Link href='/dashboard'>Logout</Link>
-					</li>
-				</ul>
+
+				{/* <div>
+					<button onClick={() => setCollapsed(!collapsed)}>
+						<RxHamburgerMenu />
+					</button>
+
+					<ul className='flex flex-col gap-4 absolute'>
+						<li>
+							<Link href='/dashboard'>Home</Link>
+						</li>
+
+						<li>
+							<Link href='/dashboard'>Students</Link>
+						</li>
+
+						<li>
+							<Link href='/dashboard'>Schedule</Link>
+						</li>
+
+						<li>
+							<Link href='/dashboard'>My Account</Link>
+						</li>
+
+						<li>
+							<Link href='/dashboard'>Logout</Link>
+						</li>
+					</ul>
+				</div> */}
 			</div>
 		</nav>
 	);
