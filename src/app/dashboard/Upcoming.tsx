6e4bc1171d4ff642f11fdf4abf91959a27aadacf
Ryan Reeves <reeves0ryan@gmail.com>
2023-06-20 19:12:13 -0700

    Add card for dashboard

--- src/app/dashboard/Upcoming.tsx
+++ src/app/dashboard/Upcoming.tsx
@@ -9,40 +9,58 @@ import {
 	StackDivider,
 	Box,
 	Text,
+	Button,
 } from '@chakra-ui/react';
+import { FaBell } from 'react-icons/fa';
 
 export default function Upcoming() {
 	return (
 		<Card>
-			<CardHeader>
-				<Heading size='md'>Client Report</Heading>
+			<CardHeader className='flex items-center'>
+				<FaBell className='mr-2' />
+
+				<Heading size='md'>Upcoming</Heading>
 			</CardHeader>
 
 			<CardBody>
-				<Stack divider={<StackDivider />} spacing='4'>
+				<Stack divider={<StackDivider />} spacing='2'>
 					<Box>
-						<Heading size='xs' textTransform='uppercase'>
-							Summary
+						<Heading size='md' textTransform='uppercase'>
+							6:30pm - 7:00pm
 						</Heading>
-						<Text pt='2' fontSize='sm'>
-							View a summary of all your clients over the last month.
-						</Text>
 					</Box>
+
 					<Box>
-						<Heading size='xs' textTransform='uppercase'>
-							Overview
+						<Heading size='s' textTransform='uppercase'>
+							Jim Bob - Guitar
 						</Heading>
+
 						<Text pt='2' fontSize='sm'>
-							Check out the overview of your clients.
+							Contact Info
 						</Text>
+
+						<div className='mt-1 mb-2'>
+							<Button size='xs' colorScheme='blue' variant='outline'>
+								student info
+							</Button>
+						</div>
 					</Box>
+
 					<Box>
-						<Heading size='xs' textTransform='uppercase'>
-							Analysis
+						<Heading size='s' textTransform='uppercase'>
+							Notes
 						</Heading>
+
 						<Text pt='2' fontSize='sm'>
-							See a detailed analysis of all your business clients.
+							Notes written by me asdkflaksjdkalsjdklasdjklasdklj aklsdjals
+							dkjasjlkdjk askdjkalsd
 						</Text>
+
+						<div className='mt-1'>
+							<Button size='xs' colorScheme='blue' variant='outline'>
+								view full notes
+							</Button>
+						</div>
 					</Box>
 				</Stack>
 			</CardBody>
